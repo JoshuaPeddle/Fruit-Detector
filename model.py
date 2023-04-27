018093608e03d60b0da07862c0c21f2864e7ec18
Joshua Peddle <joshuapeddle128@gmail.com>
2023-04-27 17:16:47 -0230

    Tweak HP tuning values

--- model.py
+++ model.py
@@ -8,13 +8,13 @@ def get_model(hp, data_augmentation, img_height, img_width, class_names):
     model.add(layers.Resizing(img_height, img_width))
     model.add(data_augmentation)
 
-    if hp: model.add(layers.Conv2D(hp.Choice('conv1', [64]), (3, 3), activation='relu', input_shape=(img_height, img_width, 3)))
+    if hp: model.add(layers.Conv2D(hp.Choice('conv1', [64,128]), (3, 3), activation='relu', input_shape=(img_height, img_width, 3)))
     else:  model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)))
 
     if hp: model.add(layers.MaxPooling2D(pool_size=hp.Choice('polling1', [ 2,4])))
     else: model.add(layers.MaxPooling2D((2, 2)))
 
-    if hp: model.add(layers.Conv2D(hp.Choice('conv2', [256]), (3, 3), activation=hp.Choice('act2', ['relu']), input_shape=(img_height, img_width, 3)))
+    if hp: model.add(layers.Conv2D(hp.Choice('conv2', [128,256]), (3, 3), activation=hp.Choice('act2', ['relu']), input_shape=(img_height, img_width, 3)))
     else: model.add(layers.Conv2D(256, (3, 3), activation='relu'))
 
     model.add(layers.MaxPooling2D((2, 2)))
@@ -22,7 +22,7 @@ def get_model(hp, data_augmentation, img_height, img_width, class_names):
     if hp: model.add(layers.Dropout(hp.Choice('sparce_dropout', [0.1])))
     else: model.add(layers.Dropout(0.1))
 
-    if hp: model.add(layers.Conv2D(hp.Choice('conv3', [512]), (3, 3), activation='relu'))
+    if hp: model.add(layers.Conv2D(hp.Choice('conv3', [256,512]), (3, 3), activation='relu'))
     else: model.add(layers.Conv2D(512, (3, 3), activation='relu'))
 
     model.add(layers.Flatten())
@@ -33,7 +33,7 @@ def get_model(hp, data_augmentation, img_height, img_width, class_names):
     if hp: model.add(layers.Dropout(hp.Choice('dense_dropout', [0.17])))
     else: model.add(layers.Dropout(0.17))
 
-    if hp: model.add(layers.Dense(len(class_names), activation=hp.Choice('finalact', ['relu', 'softmax', 'tanh'])))
+    if hp: model.add(layers.Dense(len(class_names), activation=hp.Choice('finalact', ['relu'])))
     else: model.add(layers.Dense(len(class_names), activation='softmax'))
     
 
