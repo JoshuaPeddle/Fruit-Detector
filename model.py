08c019de3e777f04fe17cde0343cd926e5164851
Joshua Peddle <joshuapeddle128@gmail.com>
2023-04-28 13:46:12 -0230

    Tune HPs

--- model.py
+++ model.py
@@ -9,30 +9,30 @@ def get_model(hp, data_augmentation, img_height, img_width, class_names):
     model.add(layers.Resizing(img_height, img_width))
     model.add(data_augmentation)
 
-    if hp: model.add(layers.Conv2D(hp.Choice('conv1', [32, 64,128]), (3, 3), activation='relu', input_shape=(img_height, img_width, 3)))
+    if hp: model.add(layers.Conv2D(hp.Choice('conv1', [64]), (3, 3), activation='relu', input_shape=(img_height, img_width, 3)))
     else:  model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)))
 
     if hp: model.add(layers.MaxPooling2D(pool_size=hp.Choice('polling1', [ 2])))
     else: model.add(layers.MaxPooling2D((2, 2)))
 
     if hp: model.add(layers.Conv2D(hp.Choice('conv2', [64, 128]), (3, 3), activation=hp.Choice('act2', ['relu']), input_shape=(img_height, img_width, 3)))
-    else: model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    else: model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
     model.add(layers.MaxPooling2D((2, 2)))
 
-    if hp: model.add(layers.Dropout(hp.Choice('sparce_dropout', [0.1, 0.15])))
+    if hp: model.add(layers.Dropout(hp.Choice('sparce_dropout', [0.1, 0.15, 0.25])))
     else: model.add(layers.Dropout(0.15))
 
-    if hp: model.add(layers.Conv2D(hp.Choice('conv3', [128, 256, 512]), (3, 3), activation='relu'))
+    if hp: model.add(layers.Conv2D(hp.Choice('conv3', [64, 128, 256, 512]), (3, 3), activation='relu'))
     else: model.add(layers.Conv2D(512, (3, 3), activation='relu'))
 
     model.add(layers.Flatten())
     
-    if hp: model.add(layers.Dense(hp.Choice('dense', [64, 128, 256]), activation='relu'))
+    if hp: model.add(layers.Dense(hp.Choice('dense', [32, 64, 128, 256]), activation='relu'))
     else: model.add(layers.Dense(128, activation='relu'))
     model.add(BatchNormalization())
-    if hp: model.add(layers.Dropout(hp.Choice('dense_dropout', [0.1, 0.17])))
-    else: model.add(layers.Dropout(0.17))
+    if hp: model.add(layers.Dropout(hp.Choice('dense_dropout', [0.1, 0.17, 0.25])))
+    else: model.add(layers.Dropout(0.25))
 
     if hp: model.add(layers.Dense(len(class_names), activation=hp.Choice('finalact', ['relu'])))
     else: model.add(layers.Dense(len(class_names), activation='softmax'))
