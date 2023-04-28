384b3b7d5c1035e4426c24e164540b918bfc43c8
Joshua Peddle <joshuapeddle128@gmail.com>
2023-04-28 13:46:02 -0230

    Up patience

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ if PLOT:
 
 #                                           CONFIGURE MODEL
 
-callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=10)
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=30)
 
 model = get_model(False, data_augmentation, img_height, img_width, class_names)
 
