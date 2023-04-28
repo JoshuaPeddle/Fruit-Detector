c86d8c3620f1e29ca1750f1079b5c840eecf7741
Joshua Peddle <joshuapeddle128@gmail.com>
2023-04-28 13:45:32 -0230

    More augmentation

--- data_augmentation.py
+++ data_augmentation.py
@@ -5,8 +5,8 @@ def get_data_augmentation():
     data_augmentation = keras.Sequential(
     [
         layers.RandomFlip("horizontal_and_vertical"),  
-        layers.RandomRotation(0.2),
-        layers.RandomContrast(0.2),
+        layers.RandomRotation(0.45),
+        layers.RandomContrast(0.5),
     ]
     )
     return data_augmentation
