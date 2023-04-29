359699cac3eb21b3afde46571c51ca6f0f6fa68f
Joshua Peddle <joshuapeddle128@gmail.com>
2023-04-29 02:08:18 -0230

    Remove unnessicary augs

--- data_augmentation.py
+++ data_augmentation.py
@@ -4,9 +4,9 @@ import keras as keras
 def get_data_augmentation():
     data_augmentation = keras.Sequential(
     [
-        layers.RandomFlip("horizontal_and_vertical"),  
-        layers.RandomRotation(0.45),
-        layers.RandomContrast(0.5),
+        #layers.RandomFlip("horizontal_and_vertical"),  
+        #layers.RandomRotation(0.45),
+        layers.RandomContrast(0.2),
     ]
     )
     return data_augmentation
