28702ea0b9461cc72060d286e24253822c0314da
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-04 23:31:06 -0230

    Add early stopping

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ if PLOT:
 
 #                                           CONFIGURE MODEL
 
-callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=3)
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=10)
 
 model = get_model(False, data_augmentation, img_height, img_width, class_names)
 
