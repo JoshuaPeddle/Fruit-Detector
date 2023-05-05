d6328a696be61c1096764681bb88a9400d2d8645
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-04 23:31:33 -0230

    Use all augmentations

--- data_augmentation.py
+++ data_augmentation.py
@@ -4,9 +4,11 @@ import keras as keras
 def get_data_augmentation():
     data_augmentation = keras.Sequential(
     [
-        #layers.RandomFlip("horizontal_and_vertical"),  
-        #layers.RandomRotation(0.45),
-        layers.RandomContrast(0.2),
+        layers.RandomFlip("horizontal_and_vertical"),  
+        layers.RandomZoom(height_factor=(0,1), fill_mode = "constant", fill_value = 1),
+        layers.RandomRotation(0.45, fill_mode = "constant", fill_value = 1),
+        layers.RandomContrast(0.1),
+        layers.RandomBrightness((-0.2,0.2), value_range=(0, 1.0)),
     ]
     )
     return data_augmentation
